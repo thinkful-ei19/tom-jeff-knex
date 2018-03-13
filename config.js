847bbfcf9db328ca1c376376b6f2e2cc4b3d9e90
Jeff Vega <jeff@intellibliss.com>
2018-03-13 10:29:41 -0700

    EX. 6 done

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 const DATABASE_URL = process.env.DATABASE_URL
                    ||  global.DATABASE_URL 
-                   || 'postgresql://localhost/dev-restaurants-app';
+                   || 'postgresql://dev:dev@localhost/dev-restaurants-app';
 
 exports.DATABASE = {
   client: 'pg',
--- drills.js
+++ drills.js
@@ -7,15 +7,63 @@ const knex = require('knex')(DATABASE);
 process.stdout.write('\x1Bc');
 
 // Sample select 
-knex
-  .select()
-  .from('restaurants')
-  .limit(2)
-  .debug(true)
-  .then(results => console.log(results));
-
-
+// knex
+//   .select()
+//   .from('restaurants')
+//   .limit(2)
+//   .debug(true)
+//   .then(results => console.log(results));
+// //
+//
 // Destroy the connection pool
-knex.destroy().then(() => {
-  console.log('database connection closed');
-});
+// knex.destroy().then(() => {
+//   console.log('database connection closed');
+// });
+
+
+//  knex
+//   .select()
+//   .table('restaurants')
+//   .debug(true)
+//   .then(results => console.log(results));
+
+
+// knex
+// .select()
+// .from('restaurants')
+// .where({cuisine:'Italian'})
+// .limit(5)
+// .debug(true)
+// .then(results => console.log(results))
+
+
+// knex
+// .select()
+// .from('restaurants')
+// .where({cuisine:'Italian'})
+// .select('id','name')
+// .limit(10)
+// .debug(true)
+// .then(results => console.log(results))
+
+
+// knex
+// .count()
+// .from('restaurants')
+// .where({cuisine:'Thai'})
+// .debug(true)
+// .then(results => console.log(results))
+
+
+// knex
+// .count()
+// .from('restaurants')
+// .debug(true)
+// .then(results => console.log(results))
+
+knex
+.count()
+.from('restaurants')
+.where({cuisine:'Thai'})
+.where({address_zipcode:'11372'})
+.then(results => console.log(results))
